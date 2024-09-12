a5de6fcddc96ea26dfb2159ee38838c8d96f2214
Andrea Aufdencamp <96022618+AAufdencamp@users.noreply.github.com>
2024-09-11 20:12:10 -0400

    Add files via upload
    100 Days of Code Python Pro Bootcamp instructor guided project.

---
+++ JSONandPasswordManager/notes.py
@@ -0,0 +1,6 @@
+#JavaScript Object Notation: one of most popular ways of transferring data. JSON has a key value pair data
+#structure
+#We can use the inbuilt JSON library
+# json.dump() to Write; inputs are what you want to dump and the file you want to dump to. Put data in as dictionary
+# json.load() to Read
+# json.update() to Update
